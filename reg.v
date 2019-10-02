d23a5b47e40805fe1e92bfcab8507ff39d8aad0a
screenflip <screenflip@gmail.com>
2019-10-02 20:38:27 +0900

    fix reg.v

--- reg.v
+++ reg.v
@@ -2,28 +2,6 @@
 
 `default_nettype none
 
-module single_reg #(LEN_REG = `LEN_WORD)
-    (input wire  in,
-
-     input  wire [LEN_REG-1:0] in_data,
-     output wire [LEN_REG-1:0] out_data,
-
-     input wire clk,
-     input wire rstn);
-
-    reg [LEN_REG-1:0] data;
-
-    assign out_data = data;
-
-    always @(posedge clk) begin
-        if (~rstn) begin
-            data <= 'b0;
-        end else if (in) begin
-            data <= in_data;
-        end
-    end
-endmodule
-
 module regs
     #(LEN_REG     = `LEN_WORD,
       NUM_REG     = `NUM_REG)
@@ -39,22 +17,27 @@ module regs
      input wire clk,
      input wire rstn);
 
-    reg  [LEN_REG-1:0] registers[NUM_REG];
+    reg [LEN_REG-1:0] registers [NUM_REG-1:0];
 
     assign drs1 = (|drs1) ? registers[drs1] : 32'b0;
+    assign drs2 = (|drs2) ? registers[drs2] : 32'b0;
 
     always @(posedge clk) begin
-        if (~rstn) begin
-            genvar i;
-            generate
-                for (i=0;i<NUM_REG;++i) begin]
-                    register[i] <= 32'b0;
-                end
-            endgenerate
-        end else if (in_flag && |drs1) begin
-            registers[ard] <= drd;
-        end
+        registers[0] <= 32'b0;
     end
+    genvar i;
+    generate
+        for (i=1;i<NUM_REG;i=i+1) begin
+            always @(posedge clk) begin
+                if (~rstn) begin
+                    registers[i] <= 32'b0;
+                end
+                else if (in_flag && ard == i) begin
+                    registers[ard] <= drd;
+                end
+            end
+        end
+    endgenerate
 endmodule
 
 `default_nettype wire
