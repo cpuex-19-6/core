2432f815698c2381271d7dde90e2cba4c3536ec4
screenflip <screenflip@gmail.com>
2019-10-22 17:26:43 +0900

    fpu.vの仕様を変更 README.mdを修正

--- fpu.v
+++ fpu.v
@@ -4,20 +4,27 @@
 
 module fpu
     (input  wire                  order, // the order for calculation
+     output wire                  accepted, // high at the clock when calculation starts
+     output wire                  done, // high at the clock when calculation completes
+
      input  wire [`LEN_FUNC3-1:0] func3,
      input  wire [`LEN_FUNC7-1:0] func7,
      input  wire [`LEN_WORD-1:0]  rs1,
      input  wire [`LEN_WORD-1:0]  rs2,
 
-     output wire                  accepted, // high at the clock calculation starts
-     output wire                  calculated, // high at the clock calculation completes
      output wire [`LEN_WORD-1:0]  rd,
      
      input  wire                  clk,
      input  wire                  rstn); // ~reset
 
-    // use registers before calculating
-    // (start calculation at the same time "clk" rises)
+    // start without storing data in registers before calculating
+    // (start calculation as soon as data come)
+    // (input data are through only a little calculation synchronized to the clock,
+    //  so fpu can calculate until the next clock.)
+
+    // fpu should not store data in registers after calculating
+    // and synchronize return data to the clock 
+    // (cpu has registers for the return data)
 
 endmodule
 
