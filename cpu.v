546dcfadc5fd2f0405c24614b6ca795ac2813309
screenflip <screenflip@gmail.com>
2019-10-31 12:53:05 +0900

    cpu.v メモリ操作の書き込み判定をさらに変更

--- cpu.v
+++ cpu.v
@@ -290,10 +290,7 @@ module cpu
                 end
                 // mem ---------------------------
                 else if (mem_de) begin
-                    mem_io <= 
-                        ((opecode_de == `OP_MEMS ) ||
-                         (opecode_de == `OP_FMEMS)   )
-                            ? 1'b1 : 1'b0;
+                    mem_io <= opecode_de[5];
                     mem_flag <= 1'b1;
                     next_pc_ew <= pc_de + 32'd4;
                     state <= `STATE_EXECUTE_WAIT;
