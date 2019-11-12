4b8dfcc552daebce8dc5cbff9f0e955c6d58a793
screenflip <screenflip@gmail.com>
2019-11-12 16:31:06 +0900

    divrem.v stage_0

--- divrem.v
+++ divrem.v
@@ -38,22 +38,50 @@ module divu_remu
     // モジュール全体で演算が終了するので、doneを上げておく
     reg stage_01;
     reg stage_12;
-    assign done = stage_12;
+    reg stage_23;
+    reg stage_34;
+    reg stage_45;
+    assign done = stage_45;
 
-    // stage_0
-
-    localparam base = 4;
+    localparam base = 4; // 2**n
     localparam base_zero = 4'b0;
+    localparam stage_max = 32 / (base * 2);
 
-    wire [`LEN_WORD+1-1:0] small_mul[2**base-1:0];
-    wire rs2_zero = |rs2;
+    wire [32+base-1:0] small_mul[stage_max:0][2**base-1:0];
 
     genvar i;
+    genvar l;
 
     generate
         for (i = 0; i < 2**base; i = i+1) begin
-            wire [`LEN_WORD+base-1:0] temp = {base_zero,rs2} * i[base-1:0];
-            assign small_mul[i] = {|temp[`LEN_WORD+base-1:`LEN_WORD],temp[`LEN_WORD-1:0]};
+            assign small_mul[0][i] = {base_zero,rs2} * i[base-1:0];
+            for (l = 0; l < stage_max; l = l+1) begin
+                temp_reg #(32+base) r_smml(1'b1,small_mul[l][i],small_mul[l+1][i],clk,rstn);
+            end
+        end
+    end
+
+    // stage_0
+
+    wire [64-1:0] rs1_64_0 == {32'b0,rs1};
+    wire [32-1:0] div_0;
+    wire [64-1:0] rem_0;
+
+    wire [32-1:0] temp_div_0[2**base+1-1:0];
+    wire [64-1:0] temp_rem_0[2**base+1-1:0];
+    assign div_rem_0[0] = {32'b0,rs1_64_0};
+    assign {div_0,rem_0} = div_rem_0[2**base];
+
+    generate
+        for (i = 0; i < 2**base; i = i+1) begin
+            assign temp_div_0[i+1] =
+                (rs1_64_0[64-1:32-base] >= small_mul[0][i])
+                    ? i : temp_div_0[i];
+            assign temp_rem_0[i+1] =
+                (rs1_64_0[64-1:32-base] >= small_mul[0][i])
+                    ? {(rs1_64_0[64-1:32-base] - small_mul[0][i]),
+                       rs1_64_0[32-base-1:0]}
+                    : temp_rem_0[i];
         end
     end
 
