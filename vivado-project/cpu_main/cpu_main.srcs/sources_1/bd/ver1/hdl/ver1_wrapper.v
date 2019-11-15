b2b7eb97cfca55dfb0843a269aaddd3f97e9432f
screenflip <screenflip@gmail.com>
2019-11-15 21:53:18 +0900

    微修正

---
+++ vivado-project/cpu_main/cpu_main.srcs/sources_1/bd/ver1/hdl/ver1_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+//Date        : Fri Nov 15 19:57:59 2019
+//Host        : LAPTOP-PU2EGKC1 running 64-bit major release  (build 9200)
+//Command     : generate_target ver1_wrapper.bd
+//Design      : ver1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module ver1_wrapper
+   ();
+
+
+  ver1 ver1_i
+       ();
+endmodule
