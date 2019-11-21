8d4a3d7283b31847e3fbff64b6c0f8a8b1ed55ba
screenflip <screenflip@gmail.com>
2019-11-21 21:27:11 +0900

    fix float_int_register_check.coe

--- bin_code/float_int_register_check.s
+++ bin_code/float_int_register_check.s
@@ -1,10 +1,19 @@
 lui	 x31 1120403456
 outw x31
+
+addi x4  x0  512
+outf f4
+
 imvf f4  x31
+outf f4
+outw x4
+
+addi x4  x0  512
 fmvi x4  f4
 outf f4
 outw x4
-addi x4  x0 0
+
+addi x4  x0  0
 ftoi x4  f4
 outf f4
 outf x4
