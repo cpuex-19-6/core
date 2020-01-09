61a1579b09fba2e16044dff19e563f064491171c
screenflip <screenflip@gmail.com>
2020-01-09 19:21:22 +0900

    execの並列化 alu_ext

--- alu.v
+++ alu.v
@@ -50,22 +50,24 @@ module alu_ext(
 
         input  wire [`LEN_FUNC3-1:0] func3,
         input  wire [`LEN_FUNC7-1:0] func7,
-
         input  wire [`LEN_WORD-1:0] rs1,
         input  wire [`LEN_WORD-1:0] rs2,
+        input  wire [`LEN_PREG_ADDR-1:0] pa_rd_in,
+
         output wire [`LEN_WORD-1:0] rd,
-     
+        output wire [`LEN_PREG_ADDR-1:0] pa_rd_out,
+
         input  wire clk,
         input  wire rstn);
 
     wire busy;
     wire next_busy = (~done) & (busy | order);
     temp_reg #(1) r_busy(1'b1, next_busy, busy, clk, rstn);
-    
+
     wire order_able = ~busy & order;
 
     // using external module
-    
+
     // divu_remu --------------------
     wire divu_remu_order = order_able &
         ((func3 == `FUNC3_REMU) |
@@ -73,12 +75,13 @@ module alu_ext(
     wire divu_remu_accepted;
     wire divu_remu_done;
     wire [32-1:0] divu_remu_rd;
+    wire [`LEN_PREG_ADDR-1:0] divu_remu_pa;
     divu_remu m_divu_remu(
         divu_remu_order, divu_remu_accepted, divu_remu_done,
-        rs1, rs2, (func3 == `FUNC3_REMU),
-        divu_remu_rd,
+        rs1, rs2, (func3 == `FUNC3_REMU), pa_rd_in,
+        divu_remu_rd, divu_remu_pa,
         clk, rstn);
-    
+
     // non-instruction
     wire error_order = order_able &
         (func3 != `FUNC3_REMU) &
@@ -86,6 +89,7 @@ module alu_ext(
     wire error_accepted = error_order;
     wire error_done = error_order;
     wire [32-1:0] error_rd = 32'b0;
+    wire [`LEN_PREG_ADDR-1:0] error_pa = `PREG_ZERO;
 
     // accepted / done / rd -----------------
     assign accepted =
@@ -102,7 +106,14 @@ module alu_ext(
         error_done     ? error_rd     : rd_buf;
     temp_reg r_rd_buf(done, next_rd_buf, rd_buf, clk, rstn);
 
+    wire [`LEN_PREG_ADDR-1:0] pa_buf;
+    wire [`LEN_PREG_ADDR-1:0] next_pa_buf =
+        divu_remu_done ? divu_remu_pa :
+        error_done     ? error_pa     : pa_buf;
+    temp_reg #(`LEN_PREG_ADDR) r_rd_buf(done, next_pa_buf, pa_buf, clk, rstn);
+
     assign rd = next_rd_buf;
+    assign pa_rd_out = next_pa_buf;
 
 endmodule
 
