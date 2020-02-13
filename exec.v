d23975c7c70f303d9d6f87aa26f6cff127dbb42c
screenflip <screenflip@gmail.com>
2020-02-13 12:17:44 +0900

    execの並列化 branch fbranch

--- exec.v
+++ exec.v
@@ -10,6 +10,38 @@ module exec
 --------------------------------
 */
 
+module branch_wrap(
+        input  wire order,
+        output wire accepted,
+        output wire done,
+
+        input  wire [`LEN_FUNC3-1:0] func3,
+        input  wire [`LEN_WORD-1:0] rs1,
+        input  wire [`LEN_WORD-1:0] rs2,
+        input  wire float,
+        output wire jump);
+    
+    // branch
+    wire ibranch_result;
+
+    branch m_br(
+        func3, d_rs1, d_rs2,
+        ibranch_result);
+
+    // fbranch
+    wire fbranch_result;
+
+    fbranch m_fbr(
+        func3, d_rs1, d_rs2,
+        fbranch_result);
+    
+    assign jump = float? fbranch_result
+                       : ibranch_result;
+    assign accepted = order;
+    assign done = order;
+    
+endmodule
+
 module exec(
         // inst_window
         input  wire order,
