10337701b5e221bb0a7d4958e8d1c1597a3c3abf
screenflip <screenflip@gmail.com>
2020-02-22 17:27:47 +0900

    exec並列化完了

--- exec.v
+++ exec.v
@@ -186,12 +186,13 @@ module exec(
     wire alu_accepted;
     wire alu_done;
     wire [32-1:0] alu_rd;
+    wire [`LEN_PREG_ADDR-1:0] alu_pa_rd_out;
 
     alu m_alu(
         alu_order, alu_accepted, alu_done,
         func3[`EX_OTH], func7[`EX_OTH],
-        d_rs1[`EX_OTH], d_rs2[`EX_OTH],
-        alu_rd,
+        d_rs1[`EX_OTH], d_rs2[`EX_OTH], pa_rd_in[`EX_OTH],
+        alu_rd, alu_pa_rd_out,
         clk, rstn);
 
     // alu_ext
@@ -200,18 +201,20 @@ module exec(
     wire alu_imm;
     wire alu_ext;
     alu_exec_type m_axt(
-        exec_type, alu, alu_std, alu_imm, alu_ext);
+        exec_type[`EX_OTH], alu, alu_std, alu_imm, alu_ext);
 
     wire alu_ext_order =
         order_able & alu_ext;
     wire alu_ext_accepted;
     wire alu_ext_done;
     wire [32-1:0] alu_ext_rd;
+    wire [`LEN_PREG_ADDR-1:0] alu_ext_pa_rd_out;
 
     alu_ext m_alu_ext(
         alu_ext_order, alu_ext_accepted, alu_ext_done,
-        func3, func7, d_rs1, d_rs2,
-        alu_ext_rd,
+        func3[`EX_OTH], func7[`EX_OTH],
+        d_rs1[`EX_OTH], d_rs2[`EX_OTH], pa_rd_in[`EX_OTH],
+        alu_ext_rd, alu_ext_pa_rd_out,
         clk, rstn);
 
     // fpu1
@@ -219,11 +222,13 @@ module exec(
     wire fpu1_accepted;
     wire fpu1_done;
     wire [32-1:0] fpu1_rd;
+    wire [`LEN_PREG_ADDR-1:0] fpu1_pa_rd_out;
 
     fpu m_fpu(
         fpu1_order, fpu1_accepted, fpu1_done,
-        func3, func7, d_rs1, d_rs2,
-        fpu1_rd,
+        func3[`EX_OTH], func7[`EX_OTH],
+        d_rs1[`EX_OTH], d_rs2[`EX_OTH], pa_rd_in[`EX_OTH],
+        fpu1_rd, fpu1_pa_rd_out,
         clk, rstn);
 
     // fpu2
@@ -231,11 +236,13 @@ module exec(
     wire fpu2_accepted;
     wire fpu2_done;
     wire [32-1:0] fpu2_rd;
+    wire [`LEN_PREG_ADDR-1:0] fpu2_pa_rd_out;
 
     fpu m_fpu(
         fpu2_order, fpu2_accepted, fpu2_done,
-        func3, func7, d_rs1, d_rs2,
-        fpu2_rd,
+        func3[`EX_OTH], func7[`EX_OTH],
+        d_rs1[`EX_OTH], d_rs2[`EX_OTH], pa_rd_in[`EX_OTH],
+        fpu2_rd, fpu2_pa_rd_out,
         clk, rstn);
 
     // fpu3
@@ -243,20 +250,23 @@ module exec(
     wire fpu3_accepted;
     wire fpu3_done;
     wire [32-1:0] fpu3_rd;
+    wire [`LEN_PREG_ADDR-1:0] fpu3_pa_rd_out;
 
     fpu m_fpu(
         fpu3_order, fpu3_accepted, fpu3_done,
-        func3, func7, d_rs1, d_rs2,
-        fpu3_rd,
+        func3[`EX_OTH], func7[`EX_OTH],
+        d_rs1[`EX_OTH], d_rs2[`EX_OTH], pa_rd_in[`EX_OTH],
+        fpu3_rd, fpu3_pa_rd_out,
         clk, rstn);
 
     // subst
     wire subst_order = order_able & exec_type[`EXEC_TYPE_SUBST];
     wire subst_accepted = subst_order;
     wire subst_done = subst_order;
-    wire [32-1:0] subst_rd = d_rs1;
+    wire [32-1:0] subst_rd = d_rs1[`EX_OTH];
+    wire [`LEN_PREG_ADDR-1:0] subst_pa_rd_out = pa_rd_in[`EX_OTH];
 
-    assign accepted =
+    assign accepted[`EX_OTH] =
         alu_accepted     |
         alu_ext_accepted |
         fpu1_accepted    |
@@ -264,7 +274,7 @@ module exec(
         fpu3_accepted    |
         subst_accepted;
 
-    assign done =
+    assign done[`WR_OTH] =
         alu_done     |
         alu_ext_done |
         fpu1_done    |
@@ -272,25 +282,16 @@ module exec(
         fpu3_done    |
         subst_done;
 
-    wire [32-1:0] rd_buf;
-    wire [32-1:0] next_rd_buf =
+    assign rd[`WR_OTH] =
         alu_done     ? alu_rd     :
         alu_ext_done ? alu_ext_rd :
         fpu1_done    ? fpu1_rd    :
         fpu2_done    ? fpu2_rd    :
         fpu3_done    ? fpu3_rd    :
         subst_done   ? subst_rd   :
-        rd_buf;
-    temp_reg r_rd_buf(1'b1, next_rd_buf, rd_buf, clk, rstn);
-
-    wire [`LEN_PREG_ADDR-1:0] pa_rd_buf;
-    wire [`LEN_PREG_ADDR-1:0] next_pa_rd_buf =
-        accepted ? pa_rd_in : pa_rd_buf;
+        32'b0;
     temp_reg #(`LEN_PREG_ADDR) r_pa_rd_buf(
-        1'b1, next_pa_rd_buf, pa_rd_buf, clk, rstn);
-
-    pack_struct_write_d_r p_write_d_r(
-        done, next_pa_rd_buf, next_rd_buf, write_d_r);
+        accepted[`EX_OTH], pa_rd_in[`EX_OTH], pa_rd_out[`WR_OTH], clk, rstn);
 
 endmodule
 
