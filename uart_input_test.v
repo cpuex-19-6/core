f05115992578265fdc83f3ccc60b60fcf32fde91
screenflip <screenflip@gmail.com>
2020-02-22 16:20:32 +0900

    uart input testの修正

--- uart_input_test.v
+++ uart_input_test.v
@@ -18,7 +18,7 @@ module uart_input_generate #(
     assign byte_data[3]=send_data[7:0];
     assign byte_data[2]=send_data[15:8];
     assign byte_data[1]=send_data[23:16];
-    assign byte_data[3]=send_data[31:24];
+    assign byte_data[0]=send_data[31:24];
 
     always @(posedge clk) begin
         if (~rstn) begin
